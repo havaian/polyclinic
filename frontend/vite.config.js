15d8b8ff9d4568170cea47c9113530f603d1320f
havaian <m.usman.work@gmail.com>
2025-04-25 15:17:49 +0500

    More changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import path from "path"
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [tailwindcss(),vue()],
