acb17a763f936548862fc4628da96e8d46b500db
havaian <m.usman.work@gmail.com>
2025-04-25 15:18:54 +0500

    Some changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [tailwindcss(),vue()],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': { find: '@', replacement: path.resolve(__dirname, '/src') }
